5ab430780ed07f5c5e344da7405963c2874db27e
mszwedo <mszwedo@unifocus.com>
2017-12-08 16:27:59 -0700

    Section 8 Working on Responsive Adjustments

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
-    <dix className={classes.Logo}>
+    <dix className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="MyBurger" />
     </dix>
 );
