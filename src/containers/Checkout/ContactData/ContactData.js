1f33a9d8877d7c3f6872a83313ce81c58a0bcfee
mszwedo <mszwedo@unifocus.com>
2018-01-05 16:36:16 -0700

    Section 13 Fixing a Bug

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -89,7 +89,8 @@ class ContactData extends Component {
                   {value: 'cheapest', displayValue: 'Cheapest'}
                ]
             },
-            value: '',
+            value: 'fastest',
+            validation: {},
             valid: true
          }
       },
