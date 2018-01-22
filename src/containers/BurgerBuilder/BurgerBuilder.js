acfd7b525986e29022666def8cb82cabaafe80ec
mszwedo <mszwedo@unifocus.com>
2018-01-22 16:44:40 -0700

    Section 17 Initializing Ingredients in the BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,6 +18,7 @@ class BurgerBuilder extends Component {
 
    componentDidMount() {
       console.log(this.props);
+      this.props.onInitIngredients();
    }
 
    updatePurchaseableState = (ingredients) => {
@@ -51,7 +52,7 @@ class BurgerBuilder extends Component {
       }
 
       let orderSummary = null;
-      let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
+      let burger = this.props.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
 
       if (this.props.ings) {
          burger = (
@@ -87,7 +88,8 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
    return {
       ings: state.ingredients,
-      price: state.totalPrice
+      price: state.totalPrice,
+      error: state.error
    };
 };
 
@@ -95,6 +97,7 @@ const mapDispatchToProps = dispatch => {
    return {
       onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
       onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+      onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients()),
    };
 };
 
