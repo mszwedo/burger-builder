5982a0d381ae53eb79a52777ba949fa6c14cc160
mszwedo <mszwedo@unifocus.com>
2018-01-23 15:20:28 -0700

    Section 17 Redirect to Improve UX

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,31 +1,36 @@
 import React, {Component} from 'react';
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
-import {Route} from "react-router-dom";
+import {Route, Redirect} from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 class Checkout extends Component {
 
    checkoutCancelledHandler = () => {
-       this.props.history.goBack();
+      this.props.history.goBack();
    };
 
    checkoutContinuedHandler = () => {
-       this.props.history.replace('/checkout/contact-data');
+      this.props.history.replace('/checkout/contact-data');
    };
 
-    render() {
-        return (
+   render() {
+      let summary = <Redirect to="/"/>;
+      if (this.props.ings) {
+         summary = (
             <div>
                <CheckoutSummary
                   ingredients={this.props.ings}
                   checkoutCancelled={this.checkoutCancelledHandler}
                   checkoutContinued={this.checkoutContinuedHandler}/>
                <Route path={this.props.match.url + '/contact-data'}
-                      component={ContactData} />
+                      component={ContactData}/>
             </div>
-        );
-    }
+         );
+      }
+
+      return summary;
+   }
 }
 
 const mapStateToProps = state => {
