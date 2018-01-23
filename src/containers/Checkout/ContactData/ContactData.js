2f6c248024e2bdd0fa0554faea2074f68091193a
mszwedo <mszwedo@unifocus.com>
2018-01-23 15:13:28 -0700

    Section 17 Working on Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -98,7 +98,6 @@ class ContactData extends Component {
          }
       },
       formIsValid: false,
-      loading: false
    };
 
    orderHandler = (event) => {
@@ -178,7 +177,7 @@ class ContactData extends Component {
             <Button btnType="Success" disabled={!this.state.formIsValid}>Order</Button>
          </form>)
       ;
-      if (this.state.loading) {
+      if (this.props.loading) {
          form = <Spinner />
       }
 
@@ -194,13 +193,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
    return {
       ings: state.ingredients,
-      price: state.totalPrice
+      price: state.totalPrice,
+      loading: state.loading
    };
 };
 
 const mapDispatchToProps = dispatch => {
    return {
-      onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData)),
+      onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData)),
    };
 };
 
