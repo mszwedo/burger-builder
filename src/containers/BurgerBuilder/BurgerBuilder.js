41092c01052e71a9e33ccab055892c3411cdabaf
mszwedo <mszwedo@unifocus.com>
2018-02-02 16:32:08 -0700

    Section 18 Forwarding Unauthenticated Users

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,7 +33,12 @@ class BurgerBuilder extends Component {
    };
 
    purchaseHandler = () => {
-      this.setState({purchasing: true});
+      if (this.props.isAuthenticated) {
+         this.setState({purchasing: true});
+      }
+      else {
+         this.props.history.push('/auth');
+      }
    };
 
    purchaseCancelHandler = () => {
@@ -65,6 +70,7 @@ class BurgerBuilder extends Component {
                   disabled={disabledInfo}
                   purchaseable={this.updatePurchaseableState(this.props.ings)}
                   ordered={this.purchaseHandler}
+                  isAuth={this.props.isAuthenticated}
                   price={this.props.price} />
             </Aux>
          );
@@ -90,7 +96,8 @@ const mapStateToProps = state => {
    return {
       ings: state.burgerBuilder.ingredients,
       price: state.burgerBuilder.totalPrice,
-      error: state.burgerBuilder.error
+      error: state.burgerBuilder.error,
+      isAuthenticated: state.auth.token !== null
    };
 };
 
