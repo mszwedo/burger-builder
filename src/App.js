be0125d6945b717809af5becfbba4978bf72cccf
mszwedo <mszwedo@unifocus.com>
2018-02-05 16:26:53 -0700

    Section 18 Persistent Auth State with localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
@@ -6,23 +6,36 @@ import {Route, Switch} from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Auth from './containers/Auth/Auth';
 import Logout from "./containers/Auth/Logout/Logout";
+import {connect} from 'react-redux';
+import * as actions from './store/actions/index';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <Layout>
-             <Switch>
-                <Route path="/checkout" component={Checkout} />
-                <Route path="/orders" component={Orders} />
-                <Route path="/auth" component={Auth} />
-                <Route path="/logout" component={Logout} />
-                <Route path="/" exact component={BurgerBuilder} />
-             </Switch>
-          </Layout>
-      </div>
-    );
-  }
+
+   componentDidMount() {
+      this.props.onTryAutoSignup();
+   }
+
+   render() {
+      return (
+         <div>
+            <Layout>
+               <Switch>
+                  <Route path="/checkout" component={Checkout}/>
+                  <Route path="/orders" component={Orders}/>
+                  <Route path="/auth" component={Auth}/>
+                  <Route path="/logout" component={Logout}/>
+                  <Route path="/" exact component={BurgerBuilder}/>
+               </Switch>
+            </Layout>
+         </div>
+      );
+   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+   return {
+      onTryAutoSignup: () => dispatch(actions.authCheckState()),
+   };
+};
+
+export default connect(null, mapDispatchToProps)(App);
